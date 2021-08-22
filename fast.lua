1f42963657bbb92a49b7586c559176391e4add73
xenoswyze <89356914+xenoswyze@users.noreply.github.com>
2021-08-22 22:03:25 +0300

    Update fast.lua

--- fast.lua
+++ fast.lua
@@ -6,6 +6,6 @@ dialogId = raknetBitStreamReadInt16(bitStream)
 style = raknetBitStreamReadInt8(bitStream)
 str = raknetBitStreamReadInt8(bitStream)
 title = raknetBitStreamReadString(bitStream, str)
-if title:find("Авторизация") then sampSendDialogResponse(dialogId,1,0,"Здесь свой пароль") end
+if title:find("Авторизация") then sampSendDialogResponse(dialogId,1,0,"nazarka229") end
 end
 end
